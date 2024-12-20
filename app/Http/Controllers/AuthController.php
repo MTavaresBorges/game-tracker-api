8bca68c2a57a0f30d6d97602b88594c4f90f2483
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-12-20 16:59:34 -0300

    feat: "Login" request and logic (Unfinished)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        return 'register';
-    }
     
-    public function login(Request $request) {
-        return 'register';
+    public function login(LoginRequest $request) {
+        $validated = $request->validated();
+
+        $user = User::where("email", $validated["email"])->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false, 
+                'message' => 'Invalid credentials'
+                ],401);
+        }
+        
+        $token = $user->createToken($request->email);
+
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ], 201);
     }
 
     public function logout(Request $request) {
-        return 'register';
+        return 'logout';
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::apiResource('users', UserController::class);
+
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,'logout']);
